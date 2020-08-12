fa8228468b09daed6090b9c7679a025b057cce66
Burakcan Sezgin <burakcan.sezgin@gmail.com>
2020-08-12 18:55:00 +0300

    Initial commit

---
+++ SmileRatingBar/src/main/java/com/brkcnszgn/smileratingbar/BaseLayout.kt
@@ -0,0 +1,39 @@
+package com.brkcnszgn.smileratingbar
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatDelegate
+
+
+open class BaseLayout<T : Drawable> @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val cache: MutableMap<String, T>
+
+    init {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
+        context.theme.applyStyle(R.style.SmileRatingTheme, false)
+        cache = mutableMapOf()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        cache.clear()
+    }
+
+    protected fun getDrawable(key: String, creator: () -> T?): T? {
+        return if (cache.containsKey(key)) {
+            cache[key]
+        } else {
+            val drawable = creator()
+            if (drawable != null) {
+                cache[key] = drawable
+            }
+            drawable
+        }
+    }
+}
---
+++ SmileRatingBar/src/main/java/com/brkcnszgn/smileratingbar/Constant.kt
@@ -0,0 +1,7 @@
+package com.brkcnszgn.smileratingbar
+object Constant {
+
+    const val ZERO_RATING = 0
+    const val MIN_RATING = 1
+    const val MAX_RATING = 5
+}
